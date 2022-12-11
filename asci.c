3b92867785580baafee7fc380e839c5362969e12
F0xNik <57321538+F0xNik@users.noreply.github.com>
2022-12-11 16:56:16 +0300

    Update asci.c

--- asci.c
+++ asci.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) { //создает файл наполненый невидимыми символами для проверки флагов e, t, v Cat
     FILE *fp;
     fp = fopen(argv[1], "w");
     for(int i = 0; i < 128; i++) {
